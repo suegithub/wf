bc3de1ae60ec5d1c9f6ccd31ca99fa625b65286f
suegithub <526913715@qq.com>
2015-01-06 17:48:19 +0800

    2015年1月6日17:48:16

--- src/com/wf/demo/Demo.java
+++ src/com/wf/demo/Demo.java
@@ -2,6 +2,10 @@ package com.wf.demo;
 
 public class Demo {
 
+	public Demo(){
+		System.out.println("初始化Master");
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is a Master!");
