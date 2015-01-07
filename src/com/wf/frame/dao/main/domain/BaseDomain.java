7930d6ae5c882941980670352d676faae27a9e19
suegithub <526913715@qq.com>
2015-01-07 14:17:08 +0800

    common-lang

---
+++ src/com/wf/frame/dao/main/domain/BaseDomain.java
@@ -0,0 +1,12 @@
+package com.wf.frame.dao.main.domain;
+
+import java.io.Serializable;
+
+//实现Serializable接口，以便是JVM能够序列化Domain实例
+public class BaseDomain  implements Serializable{
+
+	public String toString(){
+		return null;
+	}
+	
+}
