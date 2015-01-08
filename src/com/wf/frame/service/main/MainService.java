cb0438c10b0793bb8ffbfd750bd4abf076726454
suegithub <526913715@qq.com>
2015-01-08 15:09:34 +0800

    2015年1月8日15:09:30

--- src/com/wf/frame/service/main/MainService.java
+++ src/com/wf/frame/service/main/MainService.java
@@ -1,7 +1,11 @@
 package com.wf.frame.service.main;
 
+import org.springframework.stereotype.Service;
+
 import com.wf.frame.service.BaseService;
 
+@Service
 public class MainService extends BaseService {
+	
 	
 }
--- src/com/wf/frame/web/main/MainController.java
+++ src/com/wf/frame/web/main/MainController.java
@@ -3,10 +3,12 @@ package com.wf.frame.web.main;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.wf.frame.dao.main.User;
+import com.wf.frame.service.main.MainService;
 import com.wf.frame.web.BaseController;
 
 @Controller
@@ -15,6 +17,8 @@ public class MainController extends BaseController {
 
 	final static Logger log = Logger.getLogger(MainController.class);
 	
+	@Autowired
+	private MainService mainService;
 	
 	@RequestMapping("/init")
 	public String init(HttpServletRequest request, User user){
